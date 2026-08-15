7480be6289da52b3c9ca12d0f86754f3c069a871
strsbedh <108167239+strsbedh@users.noreply.github.com>
2026-08-15 13:37:05 +0530

    Create privateAgents.js

---
+++ src/utils/privateAgents.js
@@ -0,0 +1,3 @@
+// Hardcoded list of device IDs that appear ONLY on the private page
+// and are hidden from the main dashboard. Manually maintained.
+export const PRIVATE_DEVICE_IDS = ['DEV-TLSKVNLI'];
